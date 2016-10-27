f2d31631d6e22ef8490b4ece8b4491ffa64893e2
Alex Zhao <alexzhao@seas.upenn.edu>
2016-10-26 22:27:14 -0400

    huge changes to adventure.sh. Need to implement last part

--- adventure.sh
+++ adventure.sh
@@ -1,20 +1,56 @@
 #!/bin/bash
 
-# List of possible locations
-# room (need to get out. (north) --> cannot go north, south, east, west.)
-# Corridor (intersection)
-# Engine room.
-# periscope room
-# virtual time machine room
-# Armory
+#### List of possible locations ####
+# room
+# corridor intersection
+# engineRoom
+# periscopeRoom
+# timeMachineRoom
+# timeMachineRoomOutside
+# armory
 
 currLocation='room'
+
+# indicates the number of locks left on the timeMachineRoom
+# using the key and keycard when at timeMachineRoomOutside removes one lock.
+numberLocksLeft=2
+
+# TODO: Need to add a timeMachineRoomOutside location to everywhere...
 
 spacer="-----------------------------------------------------------------------"
 welcomeMessage=$"Welcome. You wake up having absolutely no recollection
 who you are. You have a throbbing headache. You’re in a room without windows,
 with its ceilings and walls completed covered in pitch black material. What
 would you like to do?"
+
+
+function displayHelp {
+  echo $spacer
+  echo "look: use this command at any point to observe the location you are in
+  around you, including potential items that you can add to your inventory.
+
+  inventory: use this command at any point to check
+  your inventory
+
+  wearing: use this command at any point to check what item(s) you are wearing
+
+  go <location>: In most parts of the game, the use of the cardinal
+  directions 'north', 'south', 'east', or 'west' is allowed. If at any point
+  this is not allowed, the terminal will return the appropriate prompt.
+
+  get <item>: Use this to add a specific item to your inventory.
+  The terminal will outline the appropriate items that are available around you
+  at each stage of the story. Simply use look to observe your environment!
+
+  inspect <item>: Use this to look more closely at an inventory item
+
+  use <item>: Use this command to use an inventory item at special points in the game
+
+  wear <item>: Use this command to wear an inventory item."
+
+  echo $spacer
+}
+
 
 
 # ##############################################################################
@@ -41,6 +77,8 @@ function initialLocationMessage {
     armory ) echo "The room is somber and silent, as if it is a symbolism of a
     great tragedy that has happened."
       ;;
+    codeRoom ) ## TODO: Do somethign.
+      ;;
     * ) echo "not a valid location"
       ;;
   esac
@@ -66,6 +104,8 @@ function goto {
           periscopeRoom ) echo "you can't go north from here"
             ;;
           timeMachineRoom ) echo "you can't go north from here"
+            ;;
+          timeMachineRoomOutside ) echo "You cannot go north from here"
             ;;
           armory ) echo "you walk back up the corridor"; initialLocationMessage corridor;
             currLocation=corridor;
@@ -94,6 +134,8 @@ function goto {
           periscopeRoom ) echo "you cannot go south from here"
             ;;
           timeMachineRoom ) echo "you cannot go south from here"
+            ;;
+          timeMachineRoomOutside ) echo "You cannot go south from here."
             ;;
           armory ) echo "you cannot go south from here"
             ;;
@@ -124,9 +166,9 @@ function goto {
           Good thing there is a door, and you quickly go through it and enter a room;"
           initialLocationMessage codeRoom; currLocation=codeRoom;
             ;;
-          periscopeRoom ) echo "you canot go east from here"
+          periscopeRoom ) echo "you cannot go east from here"
             ;;
-          ## TODO: FIX THE LOGIC O THIS.
+          ## TODO: FIX THE LOGIC TO THIS.
           timeMachineRoom )
             if [[ $(grep -oi helmet wearing | wc -l) -ne 0 ]]
             then
@@ -136,9 +178,13 @@ function goto {
               in the same place but feel several years older".
             else
               echo "you dislike the mysterious energy in the room and walk back
-              out, tracing your steps and arrive back at the somber, silent room"
+              out, tracing your steps and arrive back at the somber, silent room
+            filled with guns"
               currLocation=armory;
             fi
+            ;;
+          timeMachineRoomOutside ) echo "You walk back up the long corridor, and arrive
+          back in the armory"; currLocation=armory;
             ;;
           armory ) echo "you cannot go east from here"
             ;;
@@ -166,12 +212,23 @@ function goto {
           ;;
         timeMachineRoom ) echo "you cannot go west from here"
           ;;
-
-        # TODO: Should I add another location for "outsideTimeMachine" room?
+        timeMachineRoomOutside )
+          if [[ numberLocksLeft -ne 0 ]]
+          then
+            echo "Good try. But no matter how hard you push, twist, yell... the
+            heavy door just won't open. But you SWEAR you remember this place and
+            that there was a way to open it...";
+          else
+            "Finally, with the door unlocked, you enter the mysterious room."
+            initialLocationMessage timeMachineRoom; currLocation=timeMachineRoom;
+          fi
+          ;;
         armory ) echo "you follow the corridor and start walking. For some reason,
         this corridor seem extraaa long. After who knows how much time, you finally
         arrive at what looks like a very heavy door. You try to enter but the door
         seems locked."
+          initialLocationMessage timeMachineRoomOutside;
+          currLocation=timeMachineRoomOutside
           ;;
         codeRoom ) echo "you shudder when you think about the dead body on the
         other side of the door. After a moment of hesitation you decide to still exit.
@@ -188,7 +245,6 @@ function goto {
 }
 
 
-
 # ##############################################################################
 # function to Look around at a location
 # ##############################################################################
@@ -197,11 +253,11 @@ function lookaround {
     room ) echo "you look around. You're in what looks like a college dorm room,
     except more austere. There's a single bed pushed to one side of the room, and
     to your right there's a sofa with what looks like a dingy jacket draped on
-    top of it. You glance forward and suddnely realize that somehow you've been
+    top of it. You glance forward and suddenly realize that somehow you've been
     here before, and that what you see seems weirdly familiar: there's a desk on
     the northeast corner of the room, on which a digital clock is blinking with its
     eerie blue light: '3am...October 12th...'. Beside the clock is also a dusty
-    picture, of a beauful lady and a cute little girl... but who are they?"
+    picture, of a beautiful lady and a cute little girl... but who are they?"
       ;;
     corridor ) echo "There's no one but you. The dim lights seem to flicker with a
     constant rhythm, as if trying to tell you something. The pipes that line the
@@ -222,6 +278,10 @@ function lookaround {
     you find a single raised platform in the middle of the room. Perched
     on top fo the platform is a shiny helmet."
       ;;
+    timeMachineRoomOutside ) echo "Behind you is the eerily long corridor, and in front of
+    you is a heaviest door you've ever seen in your life. You really want to go
+    back the way you came. There must be a way to open this darn door";
+      ;;
     armory ) echo "lining the walls are rows and rows of guns, grenades, and weapons.
     But they seem to be locked behind a metal cage, which you can't seem to open."
       ;;
@@ -230,9 +290,14 @@ function lookaround {
     Have you been here before and what did you do here? The radar, panel of instruments,
     buttons and knobs seem so familiar, as if you were just here 2 days ago. You see
     a screen with the words 'HIT confirmed' flashing obnoxiously in red, and a map
-    with the US cities Los Angeles, New York and Chicago circled. You see a KeyCard
-    laying in the table, and wonder what it is used for."
+    with the US cities Los Angeles, New York and Chicago circled."
+
+    if ! $(inventoryContains "keycard");
+    then
+      echo "You see a KeyCard laying in the table, and wonder what it is used for.";
+    fi
       ;;
+
     * ) echo "error: currentLocation $currLocation is not valid"
       ;;
   esac
@@ -258,7 +323,11 @@ function inspectItem {
     vaguely familiar, as if you had seen it some where in the academy... what academy??
     were you once a soldier? On the chest are
     printed the mysterious words 'Kerensky I – Issac Assimov, submarine combat
-    division.' There also seems to be an object in the pocket..."
+    division.'"
+    if ! $(inventoryContains "key");
+    then
+      echo "There also seems to be an object in the pocket...";
+    fi
       ;;
     [Hh]elmet ) echo "The helmet looks like an ordinary motorcycle helmet, but it
     is somehow heavier and lighter at the same time, as if its weight changes
@@ -275,7 +344,10 @@ function inspectItem {
     [Cc]lock ) echo "The clock looks like an ordinary clock...except for some reason
     the second hand seems to be moving slower than usual?"
     ;;
-    * ) echo "unable to inspect $1: does not exist"
+    [Pp]icture ) echo "The lady and girl in the picture smile warmly at you, waving their
+    hands as if saying hello"
+    ;;
+    * ) echo "unable to inspect $1"
       ;;
   esac
 
@@ -301,16 +373,22 @@ function getItem {
           echo "There is no $1 in this location";
         fi
       ;;
+
     [Oo]bject )
       if [[ $(grep -oi jacket wearing | wc -l) -ne 0 ]];
       then
-        echo "The mysterious object in the jacket turned out to be a key and is now stored in your inventory.
-        You can now use key or inspect key."
-        echo 'key' >> inventory;
+        if ! $(inventoryContains "key");
+        then
+          echo "The mysterious object in the jacket turned out to be a key and is now stored in your inventory.
+          You can now use key or inspect key."
+          echo 'key' >> inventory;
+        else echo "you already have the key. Cannot get it again."
+        fi
       else
         echo "you cannot get the object without wearing the jacket"
       fi
       ;;
+
     [Hh]elmet )
       if [[ $location != timeMachineRoom ]];
       then
@@ -319,6 +397,7 @@ function getItem {
         echo "You cannot get the helmet and put it in your inventory. But while you are here, you can inspect $1 or wear $1."
       fi
       ;;
+
     [Kk]ey[Cc]ard )
       if [[ $currLocation == codeRoom ]]; then
         echo "keycard is now added to your inventory. You can now inspect $1 or use $1"
@@ -327,18 +406,60 @@ function getItem {
         echo "there item $1 does not exist in this location."
       fi
       ;;
+
     [Cc]lock )
       if [[ $currLocation == room ]]; then
         echo "The clock is now added to your inventory. You can now inspect $1 or use $1"
-        echo 'clock' >> inventory;
+        echo 'clock' >> inventory
       else
         echo "the item $1 does not exist in this location."
       fi
       ;;
+
+    [Pp]icture )
+    if [[ $currLocation == room ]]; then
+      echo "The picture is now added to your inventory. You can't explain it, but
+      somehow having the picture close to you is comforting... makes it...almost feel like home."
+      echo 'picture' >> inventory
+    else
+      echo "the item $1 does not exist in this location."
+    fi
+      ;;
+
     * ) echo "Good try, but you can't get $1";
   esac
 }
 
+## Helper function to add an item to your inventory.
+function addToInventory {
+  if [[ $(grep -oi $1 inventory | wc -l) -ne 0 ]];
+  then
+    echo "you already have $1 in your inventory. Cannot add it again"; return;
+  else
+    echo $1 >> inventory
+  fi
+}
+
+## Helper function to remove an item from your inventory, that is, use sed to
+# replace the input batter with a blank string.
+function removeFromInventory {
+  if [[ $(grep -oi $1 inventory | wc -l) -eq 0 ]];
+  then
+    echo "You do not have $1 in your inventory. Cannot remove."; return;
+  else
+
+    ## FIXME: THIS sed is NOT compatible with GNU linux. sed's i flag on mac is not
+    # portable to other linux distibutions
+    sed -i.bak "/^$1$/d" ./inventory
+    echo "$1 has been removed from your inventory"
+  fi
+}
+
+## Function to check if inventory contains an item.
+function inventoryContains {
+  if [[ $(grep -oi $1 inventory | wc -l) -ne 0 ]]; then return 0; else return 1; fi
+}
+#
 # ##############################################################################
 # Use a specific item in your inventory
 # ##############################################################################
@@ -346,22 +467,58 @@ function useItem {
   # If you don't the specified item in your inventory you cannot use it
   if [[ $(grep -oi $1 inventory | wc -l) -eq 0 ]];
   then
-    echo "you do not have item \'$1\' in your inventory"; return;
+    echo "you do not have item '$1' in your inventory so you cannot use it"; return;
   fi
 
-  ## TODO: IMPLEMENT THIS. Complete!!!!!!
   case $1 in
-    [Kk]ey ) echo "todo"
+    [Kk]ey )
+      if [[ $currLocation != timeMachineRoomOutside ]]
+      then
+        echo 'there is no use for a key here.'
+      else
+        echo 'you insert the key into the heavy door and you hear a click, as
+        if some latch has been unlocked.'
+        # if all the locks on timeMachineRoom have been unlocked, then print an
+        # appropriate welcome message.
+        numberLocksLeft=$((numberLocksLeft-1))
+        removeFromInventory 'key'
+        if [[ numberLocksLeft -eq 0 ]];
+        then
+          echo "as the lock falls away, you wonder if this mysterious heavy door has finally been unlocked";
+        else echo "hm. maybe something else is required."
+        fi
+      fi
       ;;
+
+      ## IDEA: Make it more fun so the player has to use the keycard Immediately after
+      # the key. Will this be too hard?
     [Kk]ey[Cc]ard ) echo "todo"
+      if [[ $currLocation != timeMachineRoomOutside ]]
+      then
+        echo 'there is no use for a keycard here.'
+      else
+        echo "you look around and find a hidden keypad with patterns on it. you
+        swipe. No luck the first time. You do it again. Green light. Something
+        must've worked:"
+        # if all the locks on timeMachineRoom have been unlocked, then print an
+        # appropriate welcome message.
+        numberLocksLeft=$((numberLocksLeft-1))
+        removeFromInventory 'keycard'
+        if [[ numberLocksLeft -eq 0 ]];
+        then
+          echo "along with the green light, you hear the faint sound of geras moving
+          you wonder if this mysterious heavy door has finally been unlocked";
+        else echo "hm. maybe something else is required."
+        fi
+      fi
       ;;
     [Cc]lock ) echo "todo"
+      ## TODO: USE CLOCK: After implementing the time machine room need to add this functionality.
       ;;
     * ) echo "cannot use item"
       ;;
   esac
 }
-
 
 # ##############################################################################
 # Wear either a helmet or the jacket
@@ -387,7 +544,7 @@ function wearItem {
         ## TODO: If the player does not have an alarm clock, then the player is
         # dead. Otherwise, prompt the player to use the alarm clock to slow down time.
         echo "you are now wearing the helmet. You feel a gush of energy..."
-        echo "jacket" >> wearing;
+        echo "helmet" >> wearing;
       fi
     ;;
     *) echo "You cannot wear $1"
@@ -468,7 +625,21 @@ while true; do
       # Handles the WEAR case. i.e.: Wear a wearable inventory item
       [Ww]ear ) if [[ -z "${object// }" ]]; then echo  "usage: wear <inventoryItem>"; else wearItem $object; fi;
       ;;
-
+      [Hh]elp ) displayHelp
+      ;;
+      [Ll]ocation ) echo $currLocation
+      ;;
+      [Ll]ocks ) echo $numberLocksLeft
+      ;;
+      [Cc]heat )
+        addToInventory "picture"
+        addToInventory "key"
+        addToInventory "keycard"
+        addToInventory "clock"
+        addToInventory "jacket"
+        echo "jacket" >> wearing
+        currLocation=timeMachineRoomOutside
+        ;;
       # Anything else is treated as an invalid command.
       *) echo "invalid command"
       ;;
